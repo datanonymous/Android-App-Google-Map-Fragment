8219bb2c217efbe04af33d53f2e34a856f453837
Alex Ko <ko.e.alexander@gmail.com>
2017-09-29 16:55:28 -0400

    Update NoteEditorActivity.java

--- NoteEditorActivity.java
+++ NoteEditorActivity.java
@@ -1,8 +1,10 @@
 package com.example.alex.javagooglemapapplication;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import static com.example.alex.javagooglemapapplication.NoteActivity.arrayAdapter;
 import static com.example.alex.javagooglemapapplication.NoteActivity.notes;
@@ -26,6 +29,7 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Alex Ko's Location App");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
 
@@ -52,6 +56,11 @@ public class NoteEditorActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 NoteActivity.notes.set(noteId, String.valueOf(charSequence));
                 NoteActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.alex.javagooglemapapplication", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet(NoteActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+
             }
 
             @Override
